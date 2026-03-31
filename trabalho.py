b7996e463b7c918a64797ade973b810b647eeeaa
Miguel-Galax <miguel.marques.santos@escola.pr.gov.br>
2026-03-31 11:38:24 -0300

    teste2

--- trabalho.py
+++ trabalho.py
@@ -1,19 +1,18 @@
+#1
 idade = int(input("Digite a sua idade: "))
 
 if idade >= 18:
     print("Você é maior de idade.")
 else:
     print("Você é menor de idade.")
-
+#2
     number = int(input("Digite a sua idade: "))
-
 if  number >=0 :
     print("O numero é positivo")
 else:
     print("O numero é negativo")
-
+#3
 Nota = int(input("Digite um número: "))
-
 if Nota >=6 :
     print("Você passou")
 else:
