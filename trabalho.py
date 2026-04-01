7ea780068d9a446a99b3d4da02792993578eaf28
Miguel-Galax <miguel.marques.santos@escola.pr.gov.br>
2026-04-01 12:13:45 -0300

    teste4

--- trabalho.py
+++ trabalho.py
@@ -12,8 +12,86 @@ if  number >=0 :
 else:
     print("O numero é negativo")
 #3
-Nota = int(input("Digite um número: "))
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
+
+#4
+Nota = int(input("Digite sua nota: "))
 if Nota >=6 :
     print("Você passou")
 else:
     print("Você reprovou")
+
+#5
+ temperatura= int(input("Digite a temperatura: "))
+
+if temperatura>= 30:
+    print("A temperatura está quente")
+else:
+    print("A temperatura está fria")
+
+#6
+numero2 = float(input("Digite um número: "))
+if numero2 > 10:
+    print(f"O número {numero2} é maior que 10.")
+else:
+    print(f"O número {numero2} é menor ou igual a 10.")
+
+#7
+    salario = int(input("Digite seu salario:"))
+if  salario >=3000 :
+    print("O salario é alto")
+else:
+    print("O salario é baixo")
+
+#8
+idade2= int(input("Digite a sua idade: "))
+
+if idade2 <= 12:
+    print("Sua idade é de uma criança.")
+else:
+    print("Sua idade é de um adolecente/adulto.")
+
+#9
+numero3= int(input("Digite a sua idade: "))
+
+if numero3 = 0:
+    print("Seu numero é igual a 0.")
+else:
+    print("Seu numero é diferente de 0.")
+
+#10
+nome= int(input("Digite um nome: "))
+
+if nome = Maria:
+    print("Seu nome é igual a Maria.")
+else:
+    print("Seu nome é diferente de Maria.")
+
+#11
+senha = int(input("Digite a sua senha:"))
+
+if senha = 1234:
+    print("Acesso permitido.")
+else:
+    print("Acesso negado.")
+
+#12
+numero4 = int(input("Digite um numero: "))
+
+if numero4 >= 100:
+    print("Seu numero é maior que 100.")
+else:
+    print("Seu numero é menor que 100.")
+
+#13
+numero5 = int(input("Digite a quantidade de produto: "))
+
+if numero5 >= 10:
+    print("Você recebeu um desconto.")
+else:
+    print("Você não recebeu um desconto.")
